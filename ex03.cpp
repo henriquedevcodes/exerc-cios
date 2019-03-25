ab1a90ebe8ef7ad3d5908e7035761dfed4c9d9bc
Luis Henrique <46266730+henriquedevcodes@users.noreply.github.com>
2019-03-25 17:09:59 -0300

    Update ex03.cpp

--- ex03.cpp
+++ ex03.cpp
@@ -10,16 +10,17 @@ int main() {
 	int impar = 1;
 
 	do {
-		std::cout << "Entre com um numero n: ";
+		std::cout << "Entre com um numero n inteiro positivo: ";
 		std::cin >> n;
-
-		std::cout << "Primeiros " << n << " numeros naturais impares: ";
-
-		while (i < n) {			
-			std::cout << impar << " ";
-			impar = impar + 2;
-			i++;
-		}
 	} while (n < 0);
 
+	std::cout << "Primeiros " << n << " numeros naturais impares: ";
+
+	while (i < n) {			
+		std::cout << impar << " ";
+		impar = impar + 2;
+		i++;
+	}
+	
 	return 0;
+}
